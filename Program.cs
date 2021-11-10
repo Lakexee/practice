3e49e1ea982bd9ba19bd8f509a586b40330b5e3d
Olalekan Ade <lakexee@yahoo.co.uk>
2021-11-10 20:17:47 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton employee = new Singleton();
+            employee.PrintMessage("This is the message from employee");
+
+            Singleton student = new Singleton();
+            student.PrintMessage("This is the message from student");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sample
+{
+    public class Singleton
+    {
+        public static int counter = 0;
+        public Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter value: " + counter.ToString());
+        }
+
+        public void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
